dfbfa57f6f6cb9b6689b93512bb8b15a0d69cd9e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-08-27 16:08:10 +0200

    + Added "v-for" for contacts section

--- js/script.js
+++ js/script.js
@@ -2,17 +2,12 @@ console.log("Js ok", Vue)
 
 //// Configuration to activate "Vue" on the web inspector
 Vue.config.devtools = true;
-
 //// Variable to activate "Vue"
 const app = new Vue({
     el: "#root",
     data: {
-        user: {
-            name: 'Nome Utente',
-            avatar: '_io',
-        },
-        contacts,
-    },
+        data
+    }
 });
 
 
